4d6bdce626ad3419d022181ee7bc08b0edb24519
Joaquin Ramirez <joaqu@LAPTOP-FH5918UE>
2020-03-07 03:18:54 -0600

    Updates

--- precache-manifest.8e99272381d871568a62006d8c11a6f8.js
+++ precache-manifest.8e99272381d871568a62006d8c11a6f8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3cf5a3c6a681e618fc521dd7c6d1f35f",
+    "revision": "bcaca25cb47210b9775402db3695473c",
     "url": "/Pokemon_App/index.html"
   },
   {
-    "revision": "362d7318e09fb8104bc7",
+    "revision": "c4c61229a2d75bfe3028",
     "url": "/Pokemon_App/static/css/main.4f203556.chunk.css"
   },
   {
@@ -16,15 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Pokemon_App/static/js/2.b455df51.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f857bbd45c478ab927ec",
-    "url": "/Pokemon_App/static/js/3.ec4e5704.chunk.js"
+    "revision": "c4107641461661860bb8",
+    "url": "/Pokemon_App/static/js/3.3eb41016.chunk.js"
   },
   {
-    "revision": "362d7318e09fb8104bc7",
-    "url": "/Pokemon_App/static/js/main.244dceb8.chunk.js"
+    "revision": "c4c61229a2d75bfe3028",
+    "url": "/Pokemon_App/static/js/main.468c9ab6.chunk.js"
   },
   {
-    "revision": "e8f0aef2301495a19db1",
-    "url": "/Pokemon_App/static/js/runtime-main.2d76aaa9.js"
+    "revision": "208befcc27576bd8ba0f",
+    "url": "/Pokemon_App/static/js/runtime-main.6e19cbef.js"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Pokemon_App/precache-manifest.38467783a4244e5c9cc045a5a7163eb3.js"
+  "/Pokemon_App/precache-manifest.8e99272381d871568a62006d8c11a6f8.js"
 );
 
 self.addEventListener('message', (event) => {
