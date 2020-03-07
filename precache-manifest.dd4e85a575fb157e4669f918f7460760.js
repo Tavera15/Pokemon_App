d14803d6a6bdc83c510bfc66d4ac305c795be607
Joaquin Ramirez <joaqu@LAPTOP-FH5918UE>
2020-03-07 01:56:43 -0600

    Updates

---
+++ precache-manifest.dd4e85a575fb157e4669f918f7460760.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202eb8eae977d1c4a2d92a07a754766c",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "ecace58624d7a16672ed",
+    "url": "/Pokemon/static/css/main.4f203556.chunk.css"
+  },
+  {
+    "revision": "7e14aa7e750e497823b7",
+    "url": "/Pokemon/static/js/2.b455df51.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/Pokemon/static/js/2.b455df51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32c0012fb836d34b080b",
+    "url": "/Pokemon/static/js/3.46321f01.chunk.js"
+  },
+  {
+    "revision": "ecace58624d7a16672ed",
+    "url": "/Pokemon/static/js/main.b30f37ad.chunk.js"
+  },
+  {
+    "revision": "2bfd791484d8f45b5289",
+    "url": "/Pokemon/static/js/runtime-main.2cf1ba03.js"
+  }
+]);
